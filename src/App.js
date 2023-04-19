e9fa696eb05d0825f06dd5e393f8809437aa6982
Dhineshkumaru <42208199+Dhineshkumaru@users.noreply.github.com>
2023-04-19 22:31:20 +0530

    Add files via upload
    Addressed task review comments.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Pagination from './Pagination/Pagination';
+function App() {
+  return (
+    <>
+      <div className='wrapData'>
+      <Pagination />
+      </div>
+    </>
+  );
+}
+
+export default App;
